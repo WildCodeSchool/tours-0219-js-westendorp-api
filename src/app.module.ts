da55799c15b01e41dd7e3fa7ec9d9716cd76a449
JeremyGueCo <jeremy.guerincoquereau@gmail.com>
2019-05-29 14:17:44 +0200

    DB connection OK, GET all articles

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ArticlesModule } from './articles/articles.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://admin:transformer2019@cluster0-vfkte.azure.mongodb.net/test?retryWrites=true'),
+  imports: [MongooseModule.forRoot(
+    'mongodb+srv://admin:transformer2019@cluster0-vfkte.azure.mongodb.net/Westendorp?retryWrites=true'),
     ArticlesModule],
 })
 export class AppModule {}
