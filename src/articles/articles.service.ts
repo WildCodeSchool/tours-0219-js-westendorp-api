a97580e0f7d00e131a5c4511946c868b61ac63a1
felix <felixok@live.fr>
2019-05-29 17:23:19 +0200

    fix error spaces

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -39,11 +39,9 @@ export class ArticlesService {
 
   async getById(id: string): Promise<Articles> {
     const article = await this.articlesModel.findById(id);
-
     if (!article) {
       throw new HttpException('Not found', HttpStatus.NOT_FOUND);
     }
-
     return article;
   }
 }
