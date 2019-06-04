71beb9e2eac10e8b7a2fa0cd9a0e3d526ee04358
BLOT Martial <martial.blot@outlook.fr>
2019-06-04 09:16:47 +0200

    Début partie authentification

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ArticlesModule } from './articles/articles.module';
 import { MediasModule } from './medias/medias.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [MongooseModule.forRoot(
     'mongodb+srv://admin:transformer2019@cluster0-vfkte.azure.mongodb.net/Westendorp?retryWrites=true'),
     ArticlesModule,
-    MediasModule],
+    MediasModule,
+    AuthModule],
 })
 export class AppModule {}
