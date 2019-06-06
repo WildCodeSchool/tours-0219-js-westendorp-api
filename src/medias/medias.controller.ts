24e9807700d7fab467669360894939e92b1ec502
Jérémy Foucault <jeremfouc@orange.fr>
2019-06-06 10:10:24 +0200

    fix issus guards API

--- src/medias/medias.controller.ts
+++ src/medias/medias.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Delete, Param, Post, Body, Query } from '@nestjs/common';
+import { Controller, Get, Delete, Param, Post, Body, Query, UseGuards } from '@nestjs/common';
 import { MediasService } from './medias.service';
 import { MediasDTO } from './medias.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('medias')
 export class MediasController {
@@ -16,6 +17,7 @@ export class MediasController {
     return await this.mediasService.getByType(type);
   }
   @Delete(':id')
+  @UseGuards(AuthGuard())
   async removeMedia(@Param('id') id:string) {
     return await this.mediasService.delete(id);
   }
@@ -26,6 +28,7 @@ export class MediasController {
   }
 
   @Post()
+  @UseGuards(AuthGuard())
     async  createMedia(@Body() mediasDTO: MediasDTO) {
     return await this.mediasService.create(mediasDTO);
   }
