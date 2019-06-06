65eb4864eabc9f8b1fb9c651fae101afb37f0008
JeremyGueCo <jeremy.guerincoquereau@gmail.com>
2019-06-06 10:24:47 +0200

    Passport module for Article/Media AuthGuards

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -3,10 +3,14 @@ import { ArticlesController } from './articles.controller';
 import { ArticlesService } from './articles.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { articlesSchema } from './articles.schema';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'articles', schema: articlesSchema }])],
+  imports:
+  [MongooseModule.forFeature([{ name: 'articles', schema: articlesSchema }]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [ArticlesController],
   providers: [ArticlesService],
 })
-export class ArticlesModule {}
+export class ArticlesModule { }
--- src/medias/medias.module.ts
+++ src/medias/medias.module.ts
@@ -3,10 +3,14 @@ import { MediasController } from './medias.controller';
 import { MediasService } from './medias.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { mediasSchema } from './medias.schema';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'medias', schema: mediasSchema }])],
+  imports:
+  [MongooseModule.forFeature([{ name: 'medias', schema: mediasSchema }]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [MediasController],
   providers: [MediasService],
 })
-export class MediasModule {}
+export class MediasModule { }
