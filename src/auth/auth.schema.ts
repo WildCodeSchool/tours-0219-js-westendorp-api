df0e47f03744f75cb5b3915a402f9f468fed618a
JeremyGueCo <jeremy.guerincoquereau@gmail.com>
2019-07-08 15:50:35 +0200

    Hash password to DB

--- src/auth/auth.schema.ts
+++ src/auth/auth.schema.ts
@@ -6,17 +6,34 @@ export const authSchema = new mongoose.Schema({
   password: String,
 });
 
-authSchema.pre('save', async function save(next) {
-  if (!this.isModified('password')) return next();
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    return next();
-  } catch (err) {
-    return next(err);
-  }
+authSchema.pre('save', (next) => {
+  // generate a salt
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    // hash the password using our new salt
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+      // override the cleartext password with the hashed one
+      this.password = hash;
+      next();
+    });
+  });
 });
 
-authSchema.methods.validatePassword = async function validatePassword(data) {
-  return bcrypt.compare(data, this.password);
+authSchema.pre('findOneAndUpdate', (next) => {
+  // generate a salt
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    // hash the password using our new salt
+    bcrypt.hash(this._update.password, salt, (err, hash) => {
+      if (err) return next(err);
+      // override the cleartext password with the hashed one
+      this._update.password = hash;
+      next();
+    });
+  });
+});
+
+authSchema.methods.validatePassword = function validatePassword(data) {
+  return bcrypt.compareSync(data, this.password);
 };
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,17 +10,15 @@ import { Model } from 'mongoose';
 import { Authentication } from './authentication';
 import { InjectModel } from '@nestjs/mongoose';
 import { AuthenticationCreateDTO } from './authentication.dto.create';
-import { MailerService } from '@nest-modules/mailer';
 import { AuthenticationPassDTO } from './authentication.dto.reset';
+import * as bcrypt from 'bcrypt';
 
-const user = require('./auth.schema');
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel('authentication')
     private readonly authenticationModel: Model<Authentication>,
     private readonly jwtService: JwtService,
-    private readonly mailerService: MailerService,
   ) { }
 
   async signIn(mail: string): Promise<any> {
@@ -41,11 +39,10 @@ export class AuthService {
     const userFinding = await this.authenticationModel
       .findOne({ email: partner.email })
       .exec();
-    if (userFinding.password !== partner.password) {
+    if (!bcrypt.compareSync(partner.password, userFinding.password)) {
       throw new UnauthorizedException();
-    } else {
-      return userFinding;
     }
+    return userFinding;
   }
 
   async update(email: string, authModel: AuthenticationCreateDTO) {
