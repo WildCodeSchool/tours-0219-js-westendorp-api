a91c093db853678df0eff2b4997dd9e06a2ffe8a
mdhn <maxduchan@gmail.com>
2019-07-18 11:46:57 +0200

    fix upload

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -4,25 +4,42 @@ import {
   Post,
   UseInterceptors,
   UploadedFiles,
+  UploadedFile,
   Get,
   Param,
   Res,
   Render,
   Header,
+  Req,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { readdirSync, statSync } from 'fs';
-import { join } from 'path';
-
+import { join, extname } from 'path';
+import { diskStorage } from 'multer'
 
 @Controller()
 
 export class UploadController {
 
   @Post('upload')
-  @UseInterceptors(FileInterceptor('upload'))
-  uploadFile(@UploadedFiles() file) {
-    console.log('yo');
+  // @UseInterceptors(FileInterceptor('upload'))
+  @UseInterceptors(FileInterceptor('upload', {
+    storage: diskStorage({
+      destination: process.env.UPLOAD_PATH
+      , filename: (req, file, cb) => {
+        // Generating a 32 random chars long string
+        const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('')
+        //Calling the callback passing the random name generated with the original extension name
+        cb(null, `${randomName}${extname(file.originalname)}`)
+      }
+    })
+  }))
+  uploadFile(@UploadedFile() file, @Req() req) {
+    return {
+      "uploaded": 1,
+      "fileName": file.filename,
+      "url": `${req.protocol}://${req.hostname}/images/${file.filename}`,
+    }
   }
 
   @Get('upload/browse')
